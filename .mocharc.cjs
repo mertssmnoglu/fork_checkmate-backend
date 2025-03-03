8ca770ab26c31ad9a389307fc9ac569485b02d30
Mert Şişmanoğlu <mert190737fb@gmail.com>
2025-03-04 00:25:45 +0300

    chore(test): Update mocha configuration to specify test files in package.json scripts
    - Add new test, test-unit, coverage and coverage-unit scripts
    
    Signed-off-by: Mert Şişmanoğlu <mert190737fb@gmail.com>

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,8 +1,10 @@
 module.exports = {
 	require: ["esm", "chai/register-expect.js"], // Include Chai's "expect" interface globally
-	spec: "tests/**/*.test.js", // Specify test files
 	timeout: 5000, // Set test-case timeout in milliseconds
 	recursive: true, // Include subdirectories
 	reporter: "spec", // Use the "spec" reporter
 	exit: true, // Force Mocha to quit after tests complete
+
+	//* The following spec field commented out to specify in the package.json file
+	// spec: "tests/**/*.test.js", // Specify test files
 };
